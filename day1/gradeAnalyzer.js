eca17db18e68708c3e76a599aec22fe958e34297
nazil-tntra <nazil.dhalwala@tntra.io>
2025-10-15 15:30:50 +0530

    feat: add average logic

--- day1/gradeAnalyzer.js
+++ day1/gradeAnalyzer.js
@@ -1,3 +1,36 @@
+const students = [
+{ name: "Alice", score: 85 },
+{ name: "Bob", score: 92 },
+{ name: "Charlie", score: 58 },
+{ name: "David", score: 74 }
+];
+
 const analyzeGrades = (students) => {
-    
+
+    const scores = students?.map((v,i,arr)=>{
+        return v.score;
+    })
+
+    // console.log(scores)
+
+    const average = calculateAverage(scores)
+    console.log(`Average Score: ${average}`)
 }
+
+
+function calculateAverage(values) {
+    let avg;
+
+    if(values?.length) {
+        const totalSum = values.reduce((acc,num)=>{
+            return acc + num;
+        }, 0);
+
+        avg = totalSum / values.length;
+    }
+
+    return avg;
+}
+
+
+analyzeGrades(students)
