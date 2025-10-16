9b69c3a678cbe2a879e05eea56fdd4b7744cfb41
nazil-tntra <nazil.dhalwala@tntra.io>
2025-10-16 15:55:42 +0530

    feat: product listing done

--- day2/cartCalculator.js
+++ day2/cartCalculator.js
@@ -1,3 +1,5 @@
+const container = document.getElementById("product-container") 
+
 fetch("https://fakestoreapi.com/products")
   .then((response) => {
     return response.json();
@@ -6,11 +8,27 @@ fetch("https://fakestoreapi.com/products")
     console.log(data);
 
     const products = data.map((product) => {
-      return { title: product.title, price: product.price };
+      return { title: product.title, price: product.price, image: product.image, rating: product.rating };
     });
 
-    console.log(products)
+    console.log(products);
 
 
-    
+    products.forEach((product)=>{
+        const productCard = document.createElement("div")
+        productCard.classList.add("product-card")
+
+
+        productCard.innerHTML = `
+            <img src="${product.image}" alt="${product.title}">
+            <h2 class="product-name">${product.title}</h2>
+            <div class="flex">
+                <div class="rating">${product.rating.rate}</div>
+                <p class="price">$${product.price}</p>
+            </div>
+            <button class="btn-cart">Add to Cart</button>
+        `
+
+        container.appendChild(productCard)
+    })
   });
