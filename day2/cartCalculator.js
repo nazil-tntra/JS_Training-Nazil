c7403d8f714e04b45134b9c7c0216edd685d7a08
nazil-tntra <nazil.dhalwala@tntra.io>
2025-10-17 14:58:26 +0530

    feat: Add add-to-cart functionality

--- day2/cartCalculator.js
+++ day2/cartCalculator.js
@@ -1,34 +1,71 @@
-const container = document.getElementById("product-container") 
+const container = document.getElementById("product-container");
 
-fetch("https://fakestoreapi.com/products")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
+const cart = [];
 
-    const products = data.map((product) => {
-      return { title: product.title, price: product.price, image: product.image, rating: product.rating };
-    });
-
-    console.log(products);
-
-
-    products.forEach((product)=>{
-        const productCard = document.createElement("div")
-        productCard.classList.add("product-card")
-
-
-        productCard.innerHTML = `
-            <img src="${product.image}" alt="${product.title}">
-            <h2 class="product-name">${product.title}</h2>
-            <div class="flex">
-                <div class="rating">${product.rating.rate}</div>
-                <p class="price">$${product.price}</p>
-            </div>
-            <button class="btn-cart">Add to Cart</button>
-        `
-
-        container.appendChild(productCard)
-    })
+const fetchAllProducts = () => {
+  return new Promise(async (resolve, reject) => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    resolve(data);
   });
+};
+
+const displayProducts = async () => {
+  const productsData = await fetchAllProducts();
+  // console.log(productsData);
+
+  const products = productsData.map((product) => {
+    return {
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      image: product.image,
+      rating: product.rating,
+    };
+  });
+
+  products.forEach((product) => {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    productCard.innerHTML = `
+        <img src="${product.image}" alt="${product.title}">
+        <h2 class="product-name">${product.title}</h2>
+        <div class="flex">
+            <div class="rating">${product.rating.rate}</div>
+            <p class="price">$${product.price}</p>
+        </div>
+        <button class="add-to-cart-btn" id="${product.id}">Add to Cart</button>
+    `;
+
+    container.appendChild(productCard);
+    
+    const btn = productCard.querySelector(".add-to-cart-btn");
+
+    btn.addEventListener("click",function() {
+      // console.log(product)
+      addToCart(product)
+    })
+
+  });
+
+    
+};
+
+displayProducts();
+
+function addToCart(product) {
+  const existingItem = cart.find((p) => p.id == product.id);
+
+  if(existingItem) {
+    existingItem.quantity += 1;
+  }
+  else{
+    cart.push({...product, quantity: 1});
+  }
+
+
+  localStorage.setItem("cart",JSON.stringify(cart))
+
+  console.log("Cart : ", cart)
+}
